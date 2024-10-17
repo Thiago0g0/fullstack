6acd256d46787fb6b08a4f31e3bde4f8b69b48db
THIAGO DOMINGUES GARCIA <thiago_d_garcia@estudante.sesisenai.org.br>
2024-10-17 11:57:26 -0300

    fullstack

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import Express  from 'express'
+
+const app = Express()
+
+app.get('/pegar', function (req, res){
+    res.send('enviar esta mensagem')
+})
+
+app.get('/pegar2', function (req, res){
+    res.send('esta e outra mensagem')
+})
+
+
+
+app.listen(8000)
